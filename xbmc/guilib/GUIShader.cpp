179fb9bc10cad3529791e7a81b155eea68749374
glsimon <glsimon@noreplay.com>
2026-04-10 20:07:49 +0800

    fix compile error

--- xbmc/guilib/GUIShader.cpp
+++ xbmc/guilib/GUIShader.cpp
@@ -12,11 +12,8 @@
 #include "addons/Skin.h"
 #include "guilib/GUIComponent.h"
 #include "guilib/Shader.h"
+#include "utils/GLUtils.h"
 #include "windowing/GraphicContext.h"
-
-// OpenGL includes
-#include "windowing/WindowingFactory.h"
-#include "rendering/gl/GLIncludes.h"
 
 CGUIShader::CGUIShader(int parentID, int controlID, float posX, float posY, float width, float height, const std::string& vertexShader, const std::string& fragmentShader)
   : CGUIControl(parentID, controlID, posX, posY, width, height),
